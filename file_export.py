4064225aec2f5aba0b3157049e34f55ea9b0cffb
DarkPhoeniix <marina.vovchuk777@gmail.com>
2024-08-23 22:12:59 +0300

    fixed material parsing

--- file_export.py
+++ file_export.py
@@ -31,46 +31,53 @@ def write_scene_data(context, filepath, use_some_setting):
                 write_node_data(node, filepath)
         to_json["Nodes"] = json_objects
 
-        output.write(json.dumps(to_json, indent=4))
+        output.write(json.dumps(to_json, indent=4, sort_keys=True))
     
     return {'FINISHED'}
 
 
 def write_node_data(object, filepath):   
     node_name = object.name
+    node_filepath = str(Path(filepath).parent) + '\\' + node_name + '.node'
     material_filename = node_name + '.mat'
     mesh_filename = node_name + '.mesh'
     
     # Write node data
-    node_filepath = str(Path(filepath).parent) + '\\' + node_name + '.node'
     print(f"Exporting node to {node_filepath}...")
-    with open(node_filepath, 'w', encoding='utf-8') as output:
-        to_json = { "Material": material_filename,
-                    "Mesh": mesh_filename,
-                    "Name": node_name }
-        
-        # Parse children objects
-        json_objects = []
-        for child in object.children:
-            json_objects.append(child.name + '.node')
-            write_node_data(child, filepath)
-        to_json["Nodes"] = json_objects
-            
-        output.write(json.dumps(to_json, indent=4))
     
+    to_json = { "Name": node_name }
+    
+    # Parse transform matrix
+    to_json["Transform"] = {
+        "r0": f"{object.matrix_local[0][0]} {object.matrix_local[0][1]} {object.matrix_local[0][2]} {object.matrix_local[0][3]}",
+        "r1": f"{object.matrix_local[1][0]} {object.matrix_local[1][1]} {object.matrix_local[1][2]} {object.matrix_local[1][3]}",
+        "r2": f"{object.matrix_local[2][0]} {object.matrix_local[2][1]} {object.matrix_local[2][2]} {object.matrix_local[2][3]}",
+        "r3": f"{object.matrix_local[3][0]} {object.matrix_local[3][1]} {object.matrix_local[3][2]} {object.matrix_local[3][3]}"
+    }
+        
+    # Parse children objects
+    json_objects = []
+    for child in object.children:
+        json_objects.append(child.name + '.node')
+        write_node_data(child, filepath)
+    to_json["Nodes"] = json_objects
+        
     # Select object
     bpy.context.view_layer.objects.active = object
     
     if object.type == 'MESH':
-        
         material_filepath = str(Path(filepath).parent) + '\\' + material_filename
         print(f"Exporting material to {material_filepath}...")
+        to_json["Material"] = material_filename
         write_material_data(object, material_filepath)
         
         mesh_filepath = str(Path(filepath).parent) + '\\' + mesh_filename
         print(f"Exporting mesh to {mesh_filepath}...")
+        to_json["Mesh"] = mesh_filename
         write_mesh_data(object, mesh_filepath)
-        
+    
+    with open(node_filepath, 'w', encoding='utf-8') as output:
+        output.write(json.dumps(to_json, indent=4, sort_keys=True))
 
 
 def write_mesh_data(object, filepath):
@@ -139,15 +146,30 @@ def write_mesh_data(object, filepath):
 def write_material_data(object, filepath):
     mat = object.active_material
     
+    to_json = {}
+    
     for node in mat.node_tree.nodes:
-        print(f'\ntype {node.type}\n')
         if node.type == 'TEX_IMAGE':
-            print(f'name {node.image.name}')
-            print(f'filepath {node.image.filepath}')
-        print(f'label {node.label}')
-        print(f'bl_static_type {node.bl_static_type}')
-        print(f'idname {node.idname}')
-        print(f'label {node.label}')
+            for output in node.outputs:
+                if len(output.links) == 0:
+                    continue
+                
+                link = output.links[0]
+                socket_name = link.to_socket.name
+                if socket_name == 'Base Color':
+                    to_json['Albedo'] = node.image.name
+                if socket_name == 'Metallic':
+                    to_json['Metallic'] = node.image.name
+                        
+        if node.type == 'NORMAL_MAP':
+            for input in node.inputs:
+                for link in input.links:
+                    if link.to_socket.name == 'Color':
+                        to_json['Normal'] = link.from_socket.node.image.name
+    
+    
+    with open(filepath, 'w', encoding='utf-8') as output:
+        output.write(json.dumps(to_json, indent=4, sort_keys=True))
 
 
 class SceneExporter(Operator, ExportHelper):
